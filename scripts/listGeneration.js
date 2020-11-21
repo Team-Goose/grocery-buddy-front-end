5256a568c310d46290130fc6973583f347c2f9be
keifer-schmueser <keiferrocky@gmail.com>
2020-11-21 02:40:53 -0600

    JSON shit works now and we have lists

---
+++ scripts/listGeneration.js
@@ -0,0 +1,25 @@
+const userList = document.getElementById("userList");
+let items;
+async function getJson() {
+    await fetch("scripts/items.json")
+    .then(response => {
+       return response.json();
+    }).then(function (data) {
+        items = data;
+    });
+}
+
+async function main() {
+    await getJson();
+    generateUserList();
+}
+
+function generateUserList() {
+    for (let i = 0; i < items.items.length; i++) {
+        let newElement = document.createElement("LI");
+        newElement.innerHTML = items.items[i].name;
+        userList.appendChild(newElement);
+    }
+}
+
+main();
