680f8831bc049e891ba3a3b401200248b4bafdfb
Deuce Black <deuce2black@gmail.com>
2020-11-21 01:39:04 -0600

    Toolbar stuff

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+// Vue.component('toolbar', {
+//     // props: ['full-logo'],
+//     template: '<div id="toolbar" class="tb"><div id="tbcontent" class="tbc"><div style="width: 13%;"><a href="/"><img src="assets/leaf_logo.png" style="height: 80%; margin-left: 30%; margin-top: 1%; margin-bottom: auto;"/></a></div><span class="tbspacer"></span><a href="./login" class="login">log in</a></div><div id="tblineContainer" class="tblcontainer"><div id="tbline" class="tbl"></div></div></div>',
+//     data () {
+//         return {};
+//     }
+// });
+
+// var toolbar = new Vue({
+//     el: '#toolbarContainer'
+// });
---
+++ scripts/login.js
@@ -0,0 +1,6 @@
+var title = new Vue({
+    el: '#title',
+    data: {
+        title: "Log in"
+    }
+});
