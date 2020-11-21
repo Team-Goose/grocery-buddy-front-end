b36976912f75dd48285cca2d3d1cd8e18522f348
Deuce Black <deuce2black@gmail.com>
2020-11-20 20:20:36 -0600

    Added pages
    Added first-level navigation from the home page, delocalized CSS

--- scripts/toolbar-home.js
+++ scripts/toolbar-home.js
@@ -1,14 +1,14 @@
-var toolbar = new Vue({
-    el: '#toolbar',
-    data: {
+// var toolbar = new Vue({
+//     el: '#toolbar',
+//     data: {
 
-    }
-  });
+//     }
+//   });
 
-Vue.component('todo-item', {
-// The todo-item component now accepts a
-// "prop", which is like a custom attribute.
-// This prop is called todo.
-props: ['todo'],
-template: '<li>{{ todo.text }}</li>'
-})
+// Vue.component('todo-item', {
+// // The todo-item component now accepts a
+// // "prop", which is like a custom attribute.
+// // This prop is called todo.
+// props: ['todo'],
+// template: '<li>{{ todo.text }}</li>'
+// })
